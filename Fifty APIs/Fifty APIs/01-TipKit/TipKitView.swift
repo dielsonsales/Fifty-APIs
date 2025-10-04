664403b8295f76749d9359859c3c07f007d3bb0b
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2025-10-03 22:56:29 -0300

    Add logic for showing a tip on the TipKitView

--- Fifty APIs/Fifty APIs/01-TipKit/TipKitView.swift
+++ Fifty APIs/Fifty APIs/01-TipKit/TipKitView.swift
@@ -6,13 +6,38 @@
 //
 
 import SwiftUI
+import TipKit
+
+struct MyTip: Tip {
+    var title: Text {
+        Text("Save a favorite")
+    }
+    var message: Text? {
+        Text("Add this favorite to your list of favorites")
+    }
+    var image: Image? {
+        nil
+    }
+}
 
 struct TipKitView: View {
+    let myTip = MyTip()
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            TipView(myTip, arrowEdge: .bottom)
+            Image(systemName: "star")
+                .popoverTip(myTip)
+        }
+        .task {
+            do {
+                try Tips.configure()
+            } catch {
+                print("Error configuring tips: \(error)")
+            }
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    TipKitView()
 }
