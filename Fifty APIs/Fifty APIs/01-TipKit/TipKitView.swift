b7eb585d68222b7d63e615fe1a504930082d5854
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2025-10-03 22:34:29 -0300

    Create screen to use TipKit

---
+++ Fifty APIs/Fifty APIs/01-TipKit/TipKitView.swift
@@ -0,0 +1,18 @@
+//
+//  TipKitView.swift
+//  Fifty APIs
+//
+//  Created by Dielson Sales de Carvalho on 03/10/25.
+//
+
+import SwiftUI
+
+struct TipKitView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
